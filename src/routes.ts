527205b357c5d2ea97ea2e73055115eed061d24b
miguelfiais <miguel.fiais10@gmail.com>
2024-01-23 21:31:37 -0300

    feat: create function to check if task exists

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
 import TaskController from './controllers/TaskController'
+import { verifyTask } from './middlewares/verifyTask'
 
 export const routes = Router()
 
 routes.post('/', TaskController.create)
 routes.get('/', TaskController.index)
+routes.put('/task/:id', verifyTask, TaskController.update)
+routes.delete('/task/:id', verifyTask, TaskController.destroy)
+routes.patch('/task/:id', verifyTask, TaskController.updateFavorite)
